72f575e1124794db12f629671cb3df64af0cc90c
Junior Carlos <junior-carlosdemattos@hotmail.com>
2025-08-04 11:03:08 -0300

    Att. 2 remover.js

---
+++ funcoes/remover.js
@@ -0,0 +1,31 @@
+function removerTorneio(rl, torneios, menu) {
+    if (torneios.length === 0) {
+        console.log('Nenhum torneio registrado para remover.');
+        console.log('\nPressione Enter para retornar ao menu...');
+        return rl.question('', menu(rl));
+    }
+  
+    console.log('\n=== TORNEIOS DISPONÍVEIS PARA REMOÇÃO ===');
+    torneios.forEach((torneio, index) => {
+        console.log(`${index + 1}. ID: ${torneio.id} | Nome: ${torneio.nome} | Jogo: ${torneio.jogo} | Data: ${torneio.data}`);
+    });
+  
+    rl.question('\nDigite o ID do torneio que deseja apagar: ', (idParaRemover) => {
+        const idNumerico = parseInt(idParaRemover, 10);
+  
+        const index = torneios.findIndex(torneio => torneio.id === idNumerico);
+        if (index !== -1) {
+            const [removido] = torneios.splice(index, 1);
+            console.log(`Torneio "${removido.nome}" (ID: ${removido.id}) foi removido com sucesso!`);
+        } else {
+            console.log('ID do torneio inválido ou não encontrado!');
+        }
+  
+        console.log('\nPressione Enter para voltar ao menu...');
+        rl.question('', menu(rl));
+    });
+  }
+  
+  menu(rl);
+
+  module.exports = removerTorneio;
